d9aea86246c483228ecd54dea05be5f5f661e499
Bhaktij Koli <bhaktijkoli121@gmail.com>
2021-07-29 17:11:46 +0530

    chore: updated resolvers

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,8 +1,27 @@
+import { Book, PrismaClient, User } from "@prisma/client";
+const prisma = new PrismaClient();
+
 const resolvers = {
     Query: {
         hello(): String {
             return "Hello from GraphQL";
         },
+        async users(): Promise<User[]> {
+            return await prisma.user.findMany({});
+        },
+        async books(): Promise<Book[]> {
+            return await prisma.book.findMany({});
+        }
+    },
+    Book: {
+        async author(book: Book): Promise<User | null> {
+            return await prisma.user.findUnique({ where: { id: String(book.userId) } });
+        }
+    },
+    User: {
+        async books(user: User): Promise<Book[]> {
+            return await prisma.book.findMany({ where: { userId: user.id } })
+        }
     }
 };
 
